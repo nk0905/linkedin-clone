0e14c2bfe29127d03cf50da7f7a5e66928aac54b
nk0905 <0dd2n.a0905@gmail.com>
2024-08-05 23:14:25 +0900

    clerk導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,14 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="min-h-screen flex flex-col">
         {/* Toaster */}
 
         <header className="border-b sticky top-0 bg-white z-50">
           <Header />
         </header>
 
-        <div>
+        <div className="bg-[#F4F2ED] flex-1 w-full">
           <main>{children}</main>
         </div>
       </body>
