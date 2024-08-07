60e5ff9165964d665bfa655c3541050ee0af6d88
nk0905 <0dd2n.a0905@gmail.com>
2024-08-07 22:57:05 +0900

    create user avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import { Button } from '@/components/ui/button';
+import UserInformation from '@/components/UserInformation';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>LinkedIn clone</h1>
-    </main>
+    <div className="grid">
+      <section>
+        {/* userInformation */}
+        <UserInformation />
+      </section>
+
+      <section>
+        {/* PostForm */}
+        {/* PostFeed */}
+      </section>
+
+      <section>{/* Widget */}</section>
+    </div>
   );
 }
---
+++ src/components/UserInformation.tsx
@@ -0,0 +1,18 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar';
+
+async function UserInformation() {
+  const user = await currentUser();
+  return (
+    <div>
+      <Avatar>
+        <AvatarImage src={user?.imageUrl || 'https://github.com/shadcn.png'} />
+        <AvatarFallback>
+          {user?.firstName?.charAt(0)} {user?.lastName?.charAt(0)}
+        </AvatarFallback>
+      </Avatar>
+    </div>
+  );
+}
+
+export default UserInformation;
