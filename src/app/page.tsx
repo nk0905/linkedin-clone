69fbd60af669a6326d6dda20b20b8bb9f7f47879
nk0905 <0dd2n.a0905@gmail.com>
2024-08-11 19:44:41 +0900

    add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="grid grid-cols-8 mt-5 sm:px-5">
-      <section>
+      <section className="hidden md:inline md:col-span-2">
         {/* userInformation */}
         <UserInformation />
       </section>
 
-      <section>
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+        <div className="bg-red-500">hello</div>
         {/* PostForm */}
         {/* PostFeed */}
       </section>
