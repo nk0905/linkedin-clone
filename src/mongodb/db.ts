643789c07def174d00278664530117fa3dfdb655
nk0905 <0dd2n.a0905@gmail.com>
2024-08-15 22:21:20 +0900

    add db connection error handling

--- src/mongodb/db.ts
+++ src/mongodb/db.ts
@@ -1,1 +1,9 @@
+import mongoose from 'mongoose';
+
 const connectionString = 'mongodb://localhost:27017/';
+
+if (!connectionString) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env'
+  );
+}
