eb06be24530d8410ae3c8784dbf1615471264a3b
nk0905 <0dd2n.a0905@gmail.com>
2024-08-18 22:00:38 +0900

    define post type

--- src/mongodb/models/post.ts
+++ src/mongodb/models/post.ts
@@ -1,5 +1,6 @@
 import { IUser } from '@/types/user';
-import { IComment } from './comment';
+import { IComment, ICommentBase } from './comment';
+import { Model, Schema } from 'mongoose';
 
 export interface IPostBase {
   user: IUser;
@@ -13,3 +14,38 @@ export interface IPost extends IPostBase, Document {
   createdAt: string;
   updatedAt: string;
 }
+
+// Define the document methods (for each instance of a post)
+interface IPostMethods {
+  likePost(userId: string): Promise<void>;
+  a;
+  unlikePost(userId: string): Promise<void>;
+  commentOnPost(comment: ICommentBase): Promise<void>;
+  getAllComments(): Promise<IComment[]>;
+  removePost(): Promise<void>;
+}
+
+interface IPostStatics {
+  getAllPosts(): Promise<IPostDocument[]>;
+}
+
+export interface IPostDocument extends IPost, IPostMethods {} // singular instance of a post
+interface IPostModel extends IPostStatics, Model<IPostDocument> {} // all posts
+
+const PostSchema = new Schema<IPostDocument>(
+  {
+    user: {
+      userId: { type: String, required: true },
+      userImage: { type: String, required: true },
+      firstName: { type: String, required: true },
+      lastName: { type: String },
+    },
+    text: { type: String, required: true },
+    imageUrl: { type: String },
+    comments: { type: [Schema.Types.ObjectId], ref: 'Comment', default: [] },
+    likes: { type: [String] },
+  },
+  {
+    timestamps: true,
+  }
+);
