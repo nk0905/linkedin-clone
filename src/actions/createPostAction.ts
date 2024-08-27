def4c74ad3b253675086757b2d984e926a40ce06
nk0905 <0dd2n.a0905@gmail.com>
2024-08-27 22:56:17 +0900

    createPostAction add

--- src/actions/createPostAction.ts
+++ src/actions/createPostAction.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { AddPostRequestBody } from '@/app/api/posts/route';
+import { Post } from '@/mongodb/models/post';
 import { IUser } from '@/types/user';
 import { currentUser } from '@clerk/nextjs/server';
 
@@ -11,7 +13,7 @@ export default async function createPostAction(formData: FormData) {
   }
 
   const postInput = formData.get('postInput') as string;
-  const image = formData.get('image') as string;
+  const image = formData.get('image') as File;
   let imageUrl: string | undefined;
 
   if (!postInput) {
@@ -24,4 +26,23 @@ export default async function createPostAction(formData: FormData) {
     firstName: user.firstName || '',
     lastName: user.lastName || '',
   };
+
+  try {
+    if (image.size > 0) {
+      const body: AddPostRequestBody = {
+        user: userDB,
+        text: postInput,
+        imageUrl: imageUrl,
+      };
+      await Post.create(body);
+    } else {
+      const body: AddPostRequestBody = {
+        user: userDB,
+        text: postInput,
+      };
+      await Post.create(body);
+    }
+  } catch (error: any) {
+    throw new Error('Failed to create post', error);
+  }
 }
