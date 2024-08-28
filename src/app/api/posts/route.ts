57419032e0ff8c66ffa68e2dd922cd2daed5bcad
nk0905 <0dd2n.a0905@gmail.com>
2024-08-28 22:54:55 +0900

    add post add route.ts

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,4 +1,8 @@
+import connectDB from '@/mongodb/db';
+import { IPostBase, Post } from '@/mongodb/models/post';
 import { IUser } from '@/types/user';
+import { auth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 export interface AddPostRequestBody {
   user: IUser;
@@ -6,6 +10,40 @@ export interface AddPostRequestBody {
   imageUrl?: string | null;
 }
 
-export async function POST(request: Request) {}
+export async function POST(request: Request) {
+  auth().protect(); // Protect the route with Clerk authentication
+  const { user, text, imageUrl }: AddPostRequestBody = await request.json();
 
-export async function GET() {}
+  try {
+    await connectDB();
+
+    const postData: IPostBase = {
+      user,
+      text,
+      ...(imageUrl && { imageUrl }),
+    };
+
+    const post = await Post.create(postData);
+    return NextResponse.json({ message: 'Post created successfully', post });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `An error occurred while creating the post ${error}` },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const posts = await Post.getAllPosts();
+
+    return NextResponse.json(posts);
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'An error occurred while fetching posts' },
+      { status: 500 }
+    );
+  }
+}
--- src/mongodb/db.ts
+++ src/mongodb/db.ts
@@ -20,3 +20,5 @@ const connectDB = async () => {
     console.log('Error connecting to MongoDB: ', error);
   }
 };
+
+export default connectDB;
